015b16c7477eddaabf8989afb078207c627d4874
Sasha <sashabogush@gmail.com>
2019-12-10 10:19:03 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyBody extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new Column(
+      children: [
+        new Text('Hello, World!!!'),
+        new FlatButton(
+          onPressed: () {},
+          child: Text('open site'),
+          color: Colors.red,
+          textColor: Colors.white,
+        )
+      ],
+    ));
+  }
+}
+
+void main() {
+  runApp(new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+          appBar: new AppBar(title: new Text('Flutter.com.ua')),
+          body: new MyBody())));
+}
